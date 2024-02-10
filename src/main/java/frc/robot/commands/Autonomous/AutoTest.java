332ad3dc290d17945c3cf101baffdbe81f9abd43
GusRobotics <gusroboticsinc@gmail.com>
2024-02-09 19:05:46 -0500

    with limelight

--- src/main/java/frc/robot/commands/Autonomous/AutoTest.java
+++ src/main/java/frc/robot/commands/Autonomous/AutoTest.java
@@ -13,7 +13,6 @@ import edu.wpi.first.math.kinematics.SwerveModuleState;
 import edu.wpi.first.math.trajectory.Trajectory;
 import edu.wpi.first.math.trajectory.TrajectoryConfig;
 import edu.wpi.first.math.trajectory.TrajectoryGenerator;
-import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.SwerveControllerCommand;
 import frc.robot.Constants;
